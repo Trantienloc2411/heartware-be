f3ccb3476c9819daaf0c8a421d0fcfdb8c6f066a
finn <ttloc2411@gmail.com>
2024-10-14 10:53:39 +0700

    Payment successfully

--- HeartwareManagementAPI/Program.cs
+++ HeartwareManagementAPI/Program.cs
@@ -1,6 +1,8 @@
 using BusinessObjects.Context;
 using Microsoft.EntityFrameworkCore;
+using Net.payOS;
 using Repository.Implement;
+using Service.IService;
 using Service.Services;
 
 namespace HeartwareManagementAPI;
@@ -15,6 +17,13 @@ public class Program
         builder.Services.AddAuthorization();
         builder.Services.AddControllers();
 
+        IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+
+        PayOS payOS = new PayOS(configuration["PaymentEnvironment:PAYOS_CLIENT_ID"] ?? throw new Exception("Cannot find environment"),
+                configuration["PaymentEnvironment:PAYOS_API_KEY"] ?? throw new Exception("Cannot find environment"),
+                configuration["PaymentEnvironment:PAYOS_CHECKSUM_KEY"] ?? throw new Exception("Cannot find environment"));
+            builder.Services.AddSingleton(payOS);
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddDbContext<MyDbContext>(options => 
             options.UseSqlServer(builder.Configuration.GetConnectionString("Local")));
@@ -23,6 +32,7 @@ public class Program
         builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         builder.Services.AddSwaggerGen();
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+        builder.Services.AddScoped<IPayment, Payment>();
 
         var CORS_CONFIG = "_CORS_CONFIG";
         builder.Services.AddCors(options =>
